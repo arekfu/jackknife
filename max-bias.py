532a8e059a439e9f1b12630705d723dd9aa0c129
Davide Mancusi <davide.mancusi@cea.fr>
2016-04-01 19:06:09 +0200

    Generalize the estimator

--- max-bias.py
+++ max-bias.py
@@ -9,13 +9,15 @@ n_replicas = 1000
 max_ests = []
 jack_max_ests = []
 
+estimator = np.max
+
 for irep in range(n_replicas):
     sample = np.random.uniform(size=n_sample)
 
-    max_est = np.max(sample)
+    max_est = estimator(sample)
     max_ests.append(max_est)
 
-    jack_max_est = jackknife.estimate(np.max, sample)
+    jack_max_est = jackknife.estimate(estimator, sample)
     jack_max_ests.append(jack_max_est)
 
 print('average max estimator: {}'.format(np.mean(max_ests)))
