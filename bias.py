3a08410c88283d532474218d1e3742699dbb52c3
Davide Mancusi <davide.mancusi@cea.fr>
2016-05-03 18:38:18 +0200

    Lots of work

--- bias.py
+++ bias.py
@@ -5,41 +5,75 @@ import sys
 
 import jackknife
 
-n_sample = 100
-n_replicas = 1000
+n_sample = 5
+n_replicas = 10000
 
 ests = []
 jack_ests = []
+jack_ests_sq = []
+
+jack_pseudo_means = []
+jack_pseudo_vars = []
 
 est_fun = 'max' if len(sys.argv) == 1 else sys.argv[1]
 
 if est_fun == 'max':
     estimator = np.max
-    sampler = np.random.uniform
-    expected = 1
+    sampler = lambda n : np.random.uniform(size=n)
+    expected = 1.
+    bias = 1. - 1./n_sample
 elif est_fun == 'exp':
+    x0 = 0.
+    sigma = 1.
+    lmbda = 1.
     def estimator(sample):
-        return np.exp(np.mean(sample))
-    sampler = np.random.normal
-    expected = 1
+        return np.exp(lmbda * np.mean(sample))
+    sampler = lambda n: np.random.normal(x0, sigma, size=n)
+    expected = estimator(lmbda * x0)
+    bias = expected*(estimator(0.5*lmbda*sigma**2/n_sample) - 1.)
+    expected_ss_mean = np.exp(x0*lmbda + 0.5*sigma**2*lmbda**2/(n_sample-1))
+    expected_ss_mean_sq = np.exp(2*x0*lmbda + 2*sigma**2*lmbda**2/(n_sample-1))/n_sample \
+            + np.exp(2*x0*lmbda+sigma**2*lmbda**2*(2*n_sample-3)/(n_sample-1)**2)*(n_sample-1)/n_sample
 else:
     print('unrecognized estimator')
     sys.exit(1)
 
 for irep in range(n_replicas):
-    sample = sampler(size=n_sample)
+    sample = sampler(n_sample)
 
     est = estimator(sample)
     ests.append(est)
 
     jack_est = jackknife.estimate(estimator, sample)
     jack_ests.append(jack_est)
+    jack_ests_sq.append(jack_est**2)
+
+    pseudo = jackknife.pseudovalues(estimator, sample)
+    jack_pseudo_mean = np.mean(pseudo)
+    jack_pseudo_var = np.var(pseudo, ddof=1)
+    jack_pseudo_means.append(jack_pseudo_mean)
+    jack_pseudo_vars.append(jack_pseudo_var)
 
 av_est = np.mean(ests)
-av_jack_est = np.mean(jack_ests)
+std_est = np.sqrt(np.var(ests, ddof=1) / n_replicas)
+sigmas_est = np.abs(av_est - expected) / std_est
 
-print('expected value              = {}'.format(expected))
-print('average estimator           = {}'.format(av_est))
-print('average jackknife estimator = {}'.format(av_jack_est))
-print('average estimator bias      = {:e}'.format(av_est - expected))
-print('average jackknife bias      = {:e}'.format(av_jack_est - expected))
+av_jack_est = np.mean(jack_ests)
+std_jack_est = np.sqrt(np.var(jack_ests, ddof=1) / n_replicas)
+sigmas_jack_est = np.abs(av_jack_est - expected) / std_jack_est
+
+av_jack_est_sq = np.mean(jack_ests_sq)
+
+print(u'expected value              = {:.4f}'.format(expected))
+print(u'average estimator           = {:.4f} ± {:.4f} ({:.1f} sigma)'.format(av_est, std_est, sigmas_est))
+print(u'average jackknife estimator = {:.4f} ± {:.4f} ({:.1f} sigma)'.format(av_jack_est, std_jack_est, sigmas_jack_est))
+print(u'average jackknife squared   = {:.4f}'.format(av_jack_est_sq))
+
+print()
+print(u'average pseudovalue average = {:.4f}'.format(np.mean(jack_pseudo_means)))
+print(u'sqrt average pseudovalue var= {:.4f}'.format(np.sqrt(np.mean(jack_pseudo_vars))))
+
+print()
+print(u'expected bias               = {:.4e}'.format(bias))
+print(u'average estimator bias      = {:.4e}'.format(av_est - expected))
+print(u'average jackknife bias      = {:.4e}'.format(av_jack_est - expected))
--- jackknife.py
+++ jackknife.py
@@ -6,21 +6,35 @@ def subsamples(sample):
         subsample = [ x for j, x in enumerate(sample) if i!=j ]
         yield subsample
 
+def subsample_means(estimator, sample):
+    return np.array([ estimator(sub) for sub in subsamples(sample) ])
+
 def subsample_mean(estimator, sample):
-    ests = [ estimator(sub) for sub in subsamples(sample) ]
-    return np.mean(ests)
+    return np.mean(subsample_means(estimator, sample))
 
 def bias(estimator, sample):
     n = len(sample)
     bias_est = (n-1)*(subsample_mean(estimator, sample) - estimator(sample))
     return bias_est
 
-def estimate(estimator, sample):
+def pseudovalues(estimator, sample):
+    """Returns the jackknife pseudovalues"""
+
+    n = len(sample)
+    ss_means = subsample_means(estimator, sample)
+    pseudos = n*estimator(sample) - (n-1)*ss_means
+    return pseudos
+
+def estimate(estimator, sample, with_subsample_mean=False):
     """Returns the jackknife estimate of the mean
 
     This is equal to the mean minus the jackknife estimate of the bias.
     """
 
     n = len(sample)
-    mean_est = n*estimator(sample) - (n-1)*subsample_mean(estimator, sample)
-    return mean_est
+    ss_mean = subsample_mean(estimator, sample)
+    mean_est = n*estimator(sample) - (n-1)*ss_mean
+    if with_subsample_mean:
+        return mean_est, ss_mean
+    else:
+        return mean_est
